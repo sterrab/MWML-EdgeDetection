f6e4dbb26d13980d94187c3d1a006292b1347842
Soraya Terrab <sorayaterrab@gmail.com>
2022-03-20 11:45:38 -0600

    Updated DG coefficient matrix to call for degree_x=0 and degree_y=0 modes

--- FilteredImageData.py
+++ FilteredImageData.py
@@ -9,38 +9,38 @@ def compute_order2_modes_from_filtered_image(DG_coeffs, height, width):
     for pix_y in range(1, int(height - 1)):
         for pix_x in range(1, int(width - 1)):
             # Mode (kx, ky) = (0, 0) - New Average from Filtered Data
-            filtered_modes[0, 0, pix_y, pix_x] = 9 / 16 * DG_coeffs[pix_y, pix_x] + \
-                                                 3 / 32 * (DG_coeffs[pix_y - 1, pix_x] +
-                                                           DG_coeffs[pix_y + 1, pix_x] +
-                                                           DG_coeffs[pix_y, pix_x - 1] +
-                                                           DG_coeffs[pix_y, pix_x + 1]) + \
-                                                 1 / 64 * (DG_coeffs[pix_y - 1, pix_x - 1] +
-                                                           DG_coeffs[pix_y + 1, pix_x - 1] +
-                                                           DG_coeffs[pix_y - 1, pix_x + 1] +
-                                                           DG_coeffs[pix_y + 1, pix_x + 1])
+            filtered_modes[0, 0, pix_y, pix_x] = 9 / 16 * DG_coeffs[0, 0, pix_y, pix_x] + \
+                                                 3 / 32 * (DG_coeffs[0, 0, pix_y - 1, pix_x] +
+                                                           DG_coeffs[0, 0, pix_y + 1, pix_x] +
+                                                           DG_coeffs[0, 0, pix_y, pix_x - 1] +
+                                                           DG_coeffs[0, 0, pix_y, pix_x + 1]) + \
+                                                 1 / 64 * (DG_coeffs[0, 0, pix_y - 1, pix_x - 1] +
+                                                           DG_coeffs[0, 0, pix_y + 1, pix_x - 1] +
+                                                           DG_coeffs[0, 0, pix_y - 1, pix_x + 1] +
+                                                           DG_coeffs[0, 0, pix_y + 1, pix_x + 1])
             # Mode (kx, ky) = (1, 0)
-            filtered_modes[1, 0, pix_y, pix_x] = 0.10825317547305482457 * (DG_coeffs[pix_y, pix_x + 1]
-                                                             - DG_coeffs[pix_y, pix_x - 1]) + \
-                                                 0.018042195912175804096 * (DG_coeffs[pix_y - 1, pix_x + 1]
-                                                              - DG_coeffs[pix_y - 1, pix_x - 1]
-                                                              + DG_coeffs[pix_y + 1, pix_x + 1]
-                                                              - DG_coeffs[pix_y + 1, pix_x - 1])
+            filtered_modes[1, 0, pix_y, pix_x] = 0.10825317547305482457 * (DG_coeffs[0, 0, pix_y, pix_x + 1]
+                                                             - DG_coeffs[0, 0, pix_y, pix_x - 1]) + \
+                                                 0.018042195912175804096 * (DG_coeffs[0, 0, pix_y - 1, pix_x + 1]
+                                                              - DG_coeffs[0, 0, pix_y - 1, pix_x - 1]
+                                                              + DG_coeffs[0, 0, pix_y + 1, pix_x + 1]
+                                                              - DG_coeffs[0, 0, pix_y + 1, pix_x - 1])
             # Mode (kx, ky) = (0, 1)
-            filtered_modes[0, 1, pix_y, pix_x] = 0.10825317547305482457 * (DG_coeffs[pix_y + 1, pix_x]
-                                                             - DG_coeffs[pix_y - 1, pix_x]) + \
-                                                 0.018042195912175804096 * (DG_coeffs[pix_y + 1, pix_x - 1]
-                                                              - DG_coeffs[pix_y - 1, pix_x - 1]
-                                                              + DG_coeffs[pix_y + 1, pix_x + 1]
-                                                              - DG_coeffs[pix_y - 1, pix_x + 1])
+            filtered_modes[0, 1, pix_y, pix_x] = 0.10825317547305482457 * (DG_coeffs[0, 0, pix_y + 1, pix_x]
+                                                             - DG_coeffs[0, 0, pix_y - 1, pix_x]) + \
+                                                 0.018042195912175804096 * (DG_coeffs[0, 0, pix_y + 1, pix_x - 1]
+                                                              - DG_coeffs[0, 0, pix_y - 1, pix_x - 1]
+                                                              + DG_coeffs[0, 0, pix_y + 1, pix_x + 1]
+                                                              - DG_coeffs[0, 0, pix_y - 1, pix_x + 1])
             # Mode (kx, ky) = (1, 1)
-            filtered_modes[1, 1, pix_y, pix_x] = 1 / 48 * (DG_coeffs[pix_y + 1, pix_x + 1]
-                                                           + DG_coeffs[pix_y - 1, pix_x - 1]
-                                                           - DG_coeffs[pix_y + 1, pix_x - 1]
-                                                           - DG_coeffs[pix_y - 1, pix_x + 1])
+            filtered_modes[1, 1, pix_y, pix_x] = 1 / 48 * (DG_coeffs[0, 0, pix_y + 1, pix_x + 1]
+                                                           + DG_coeffs[0, 0, pix_y - 1, pix_x - 1]
+                                                           - DG_coeffs[0, 0, pix_y + 1, pix_x - 1]
+                                                           - DG_coeffs[0, 0, pix_y - 1, pix_x + 1])
     # Boundary pixels
-    filtered_modes[0, 0, 0, :] = DG_coeffs[0, :]
-    filtered_modes[0, 0, -1, :] = DG_coeffs[-1, :]
-    filtered_modes[0, 0, :, 0] = DG_coeffs[:, 0]
-    filtered_modes[0, 0, :, -1] = DG_coeffs[:, -1]
+    filtered_modes[0, 0, 0, :] = DG_coeffs[0, 0, 0, :]
+    filtered_modes[0, 0, -1, :] = DG_coeffs[0, 0, -1, :]
+    filtered_modes[0, 0, :, 0] = DG_coeffs[0, 0, :, 0]
+    filtered_modes[0, 0, :, -1] = DG_coeffs[0, 0, :, -1]
 
     return filtered_modes
